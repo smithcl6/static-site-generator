d6fc7dff3e1c23e7534bfe42fc885750be6f95c5
Christopher Smith <hyrule1414@gmail.com>
2026-05-02 02:20:12 -0400

    Added text_to_html, which translates TextType to LeafNode.

---
+++ src/test_text_to_html.py
@@ -0,0 +1,21 @@
+import unittest
+from textnode import TextNode, TextType
+from text_to_html import text_node_to_html_node
+
+class TestTypeToLeaf(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("Website", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.props_to_html(), ' href="https://www.google.com"')
+
+    def test_italic_and_bold(self):
+        italic = TextNode("Click Me", TextType.ITALIC)
+        bold = TextNode("Click Me", TextType.BOLD)
+        self.assertNotEqual(italic, bold)
