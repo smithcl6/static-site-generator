19f776f9063dc0db661cea46e23af77715ad1819
Christopher Smith <hyrule1414@gmail.com>
2026-05-04 19:03:40 -0400

    Content from static is now copied into public.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
-from textnode import TextNode, TextType
+import os, shutil
+
+STATIC = "./static"
+PUBLIC = "./public"
+
+def content_to_destination(directory:list[str]|None, path:str):
+    if not directory:
+        print("Directory is empty")
+        return
+    for item in directory:
+        # current_path does not include ./static or ./public
+        current_path = os.path.join(path, item)
+        static_path = os.path.join(STATIC, current_path)
+        public_path = os.path.join(PUBLIC, current_path)
+        if os.path.isdir(static_path):
+            os.mkdir(public_path)
+            print(f"Updated directory structure: {public_path}")
+            content_to_destination(os.listdir(static_path), current_path)
+        else:
+            shutil.copy(static_path, public_path)
+            print(f"Added {item} to {public_path}")
 
 def main():
-    text_node = TextNode("DMC2 IS BEST", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    if os.path.exists(PUBLIC):
+        shutil.rmtree(PUBLIC)
+
+    os.mkdir(PUBLIC)
+    
+    content_to_destination(os.listdir(STATIC), "")
 
 if __name__ == "__main__":
     main()
