0d6a2abd9fdcdaa99acc7154a85c527473b61503
Christopher Smith <hyrule1414@gmail.com>
2026-05-03 22:13:08 -0400

    Able to identify block type based on block text.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,15 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown:str)->list[str]:
     blocks = markdown.split("\n\n")
     blocks = list(map(lambda block: block.strip(), blocks))
@@ -6,3 +18,34 @@ def markdown_to_blocks(markdown:str)->list[str]:
         if block != "":
             result.append(block)
     return result
+
+def block_to_block_type(block:str)->BlockType:
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    elif block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    lines = block.split("\n")
+
+    if block.startswith(">"):
+        for line in lines:
+            if line.startswith(">") and len(line) > 1:
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        for line in lines:
+            if line.startswith("- "):
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        for i in range(0, len(lines)):
+            if lines[i].startswith(f"{i+1}. "):
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -50,3 +50,66 @@ This is a silly use of space.
 
     def test_markdown_to_blocks_empty(self):
         self.assertListEqual(markdown_to_blocks(""), [])
+
+    def test_block_to_block_type_standard(self):
+        blocks:list[str] = [
+            "## This is a heading",
+            '```\nprint("hello world")\n```',
+            "> This is a quote\n>that spans multiple lines",
+            "- item one\n- item two\n- item three",
+            "1. first item\n2. second item\n3. third item",
+            "This is just normal text."
+        ]
+        expected_types:list[BlockType] = [
+            BlockType.HEADING,
+            BlockType.CODE,
+            BlockType.QUOTE,
+            BlockType.UNORDERED_LIST,
+            BlockType.ORDERED_LIST,
+            BlockType.PARAGRAPH
+        ]
+        self.assertListEqual(list(map(block_to_block_type, blocks)), expected_types)
+
+    def test_block_to_block_type_headings(self):
+        blocks:list[str] = [
+            "# h1",
+            "## h2",
+            "### h3",
+            "#### h4",
+            "##### h5",
+            "###### h6",
+            "####### h7...?",
+            "#broken",
+            "# #actually h1",
+            " # broken",
+        ]
+        expected_types:list[BlockType] =[
+            BlockType.HEADING,
+            BlockType.HEADING,
+            BlockType.HEADING,
+            BlockType.HEADING,
+            BlockType.HEADING,
+            BlockType.HEADING,
+            BlockType.PARAGRAPH,
+            BlockType.PARAGRAPH,
+            BlockType.HEADING,
+            BlockType.PARAGRAPH,
+        ]
+        self.assertListEqual(list(map(block_to_block_type, blocks)), expected_types)
+
+    def test_block_to_block_type_ordered_list(self):
+        blocks:list[str] = [
+            "1. Valid\n2. List",
+            "2. Not\n3. Valid",
+            "2. Also\n1. Invalid",
+            "1.Again invalid",
+            "1. But this is okay."
+        ]
+        expected_types:list[BlockType] = [
+            BlockType.ORDERED_LIST,
+            BlockType.PARAGRAPH,
+            BlockType.PARAGRAPH,
+            BlockType.PARAGRAPH,
+            BlockType.ORDERED_LIST,
+        ]
+        self.assertListEqual(list(map(block_to_block_type, blocks)), expected_types)
